86a5081784306dc3fddc729372a74acb8872ba14
vittoriom94 <vimattei@hotmail.it>
2018-11-07 12:08:25 +0100

    interfaccia metodi set e get chiave

---
+++ src/progetto_2/KeyRing.java
@@ -0,0 +1,19 @@
+package progetto_2;
+
+
+import java.security.Key;
+
+public class KeyRing {
+
+    public Key getKey(String id){
+        return null;
+    }
+
+    public void saveKey(Key k, String id){
+
+    }
+
+    public void saveKeyring(){
+
+    }
+}
